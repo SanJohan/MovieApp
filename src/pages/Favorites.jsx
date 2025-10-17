5316fd8da362dd39962c26095da17dddbd025f1f
Diego Dev <tuemail@example.com>
2025-10-17 15:05:32 -0500

    version semicompletada

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,14 @@
+import '../css/Favorites.css'
+
+function Favorites() {
+  return (
+    <>
+      <div className="favorites-empty">
+        <h1>No favorites movies yet</h1>
+        <p>Start adding movies to your favorites and they will appear here</p>
+      </div>
+    </>
+  )
+}
+
+export default Favorites;
---
+++ src/services/api.js
@@ -0,0 +1,19 @@
+const API_KEY = "7a327f1c2019f64f270ccbad6686d082";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  console.log(data);
+  return data.results;
+};
